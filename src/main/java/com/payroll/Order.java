3ab6f71d8748d52cef131684516a54fa394c576b
BlueWorldInc <keamybakunin@gmail.com>
2021-02-07 16:08:56 +0100

    fin du tutoriel

--- src/main/java/com/payroll/Order.java
+++ src/main/java/com/payroll/Order.java
@@ -2,13 +2,14 @@ package com.payroll;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "CUSTOMER_ORDER")
 class Order {
 
-	private @GeneratedValue Long id;
+	private @Id @GeneratedValue Long id;
 	private String description;
 	private Status status;
 
--- src/main/java/com/payroll/OrderController.java
+++ src/main/java/com/payroll/OrderController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -78,9 +79,26 @@ class OrderController {
 			 + " status"));
 	}
 
-	@GetMapping("/orderss")
-	String complete(Long id) {
-		return "Bravo vous avez tout cassez et votre id est le: " + id;
+
+	@PutMapping("/orders/{id}/complete")
+	ResponseEntity<?> complete(@PathVariable Long id) {
+
+		Order order = orderRepository.findById(id)
+			.orElseThrow(() -> new OrderNotFoundException(id));
+
+		if (order.getStatus() == Status.IN_PROGRESS) {
+			order.setStatus(Status.COMPLETED);
+			return ResponseEntity.ok(assembler.toModel(orderRepository.save(order)));
+		}
+
+		return ResponseEntity
+			.status(HttpStatus.METHOD_NOT_ALLOWED)
+			.header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE)
+			.body(Problem.create()
+				.withTitle("Method not allowed")
+				.withDetail("You can't complete an order that is in the " +
+				order.getStatus() + " status"));
+
 	}
 
 }
